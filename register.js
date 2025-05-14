ed638947a0833162118f4a5bd74988366c069a51
Sharrshan <harrshan0426@gmail.com>
2025-05-14 23:53:11 +0530

    added all files

---
+++ register.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBhQNQ4Pf8GLFkgWA7p6QbuArgKTXQB4O4",
+    authDomain: "parent-sphere.firebaseapp.com",
+    projectId: "parent-sphere",
+    storageBucket: "parent-sphere.firebasestorage.app",
+    messagingSenderId: "52658078544",
+    appId: "1:52658078544:web:337ed739d4bed5b0886255"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
